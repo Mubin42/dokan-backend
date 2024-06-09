74ec2031d55a51c59c1c6f001e235644c5ca5cc8
Kazi Ehsanul Mubin <kazimubin.46@gmail.com>
2024-06-09 21:03:37 +0600

    MongoDB connection established

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 
 import { ConfigsModule } from './configs/configs.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [ConfigsModule],
+  imports: [MongooseModule.forRoot(process.env.DB_URL), ConfigsModule],
   controllers: [],
   providers: [],
 })
